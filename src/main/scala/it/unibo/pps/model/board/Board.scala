fc61d989b17d482ee7ac942cd4bacf85e4ab5e15
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-06-30 00:04:46 +0200

    Remove curly brackets

--- src/main/scala/it/unibo/pps/model/board/Board.scala
+++ src/main/scala/it/unibo/pps/model/board/Board.scala
@@ -9,7 +9,7 @@ trait Board:
   def disks: HashMap[Position, Disk]
   
 object Board:
-  def apply(shape: Shape, size: Int, disks: HashMap[Position, Disk]): Board = {
+  def apply(shape: Shape, size: Int, disks: HashMap[Position, Disk]): Board =
     disks match
       case d if d.isEmpty =>
         val leftCenter = size / 2
@@ -22,7 +22,6 @@ object Board:
         )
         new StandardBoard(shape, size, initialDisks)
       case _ => new StandardBoard(shape, size, disks)
-  }
 
   private class StandardBoard(override val shape: Shape, 
                               override val size: Int, 
