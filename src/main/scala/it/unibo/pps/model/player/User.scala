c341a12cf15ff5c8456a402c507a0a50ddbf4a38
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-01 16:19:56 +0200

    Create PlayerTest
    and its minimal implementation to pass the test.
    Also refactor User to extend Player trait.

--- src/main/scala/it/unibo/pps/model/player/User.scala
+++ src/main/scala/it/unibo/pps/model/player/User.scala
@@ -3,6 +3,6 @@ package it.unibo.pps.model.player
 import it.unibo.pps.utils.Color
 import it.unibo.pps.model.placementStrategy.UserPlacementStrategy
 
-case class User():
-  def color: Color = Color.Black
-  def getPlacementStrategy: UserPlacementStrategy = UserPlacementStrategy()
+case class User() extends Player:
+  override def color: Color = Color.Black
+  override def getPlacementStrategy: UserPlacementStrategy = UserPlacementStrategy()
---
+++ src/test/scala/it/unibo/pps/model/PlayerTest.scala
@@ -0,0 +1,15 @@
+package it.unibo.pps.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.mockito.MockitoSugar.mock
+import org.mockito.IdiomaticMockito.returns
+
+import it.unibo.pps.model.player.{Player, User, Opponent}
+import it.unibo.pps.model.placementStrategy.{PlacementStrategy, UserPlacementStrategy, OpponentPlacementStrategy}
+
+class PlayerTest extends AnyFlatSpec:
+
+  "A User" should "return the correct placement strategy" in:
+    val user: Player = User()
+    val strategy = user.getPlacementStrategy
+    assert(strategy.isInstanceOf[UserPlacementStrategy])
