44a224c94e04f585848ab7bcbc0a162af681dc40
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-01 20:31:20 +0200

    Rename tests variable

--- src/test/scala/it/unibo/pps/model/BoardTest.scala
+++ src/test/scala/it/unibo/pps/model/BoardTest.scala
@@ -9,10 +9,10 @@ import org.scalatest.matchers.should.Matchers.{should, shouldBe, empty, be, equa
 class BoardTest extends AnyFlatSpec:
   val BOARD_SIZE = 8
   val BOARD_SHAPE = Shape.Square(BOARD_SIZE)
-  val STEP = 1
+  val DISTANCE = 1
 
-  val left: Int = BOARD_SIZE / 2 - STEP
-  val right: Int = left + STEP
+  val left: Int = BOARD_SIZE / 2 - DISTANCE
+  val right: Int = left + DISTANCE
   
   "A Board without disks" should "initialize itself with the disks in the correct positions" in:
     val board = Board(BOARD_SHAPE, BOARD_SIZE)
@@ -39,16 +39,16 @@ class BoardTest extends AnyFlatSpec:
       Position(left, right) -> Disk(Color.Black),
       Position(right, left) -> Disk(Color.Black),
       Position(right, right) -> Disk(Color.White),
-      Position(right, right + STEP) -> Disk(Color.White),
-      Position(right, right + STEP + STEP) -> Disk(Color.White),
-      Position(right, right + STEP + STEP + STEP) -> Disk(Color.White)
+      Position(right, right + DISTANCE) -> Disk(Color.White),
+      Position(right, right + DISTANCE + DISTANCE) -> Disk(Color.White),
+      Position(right, right + DISTANCE + DISTANCE + DISTANCE) -> Disk(Color.White)
     )
     val board = Board(BOARD_SHAPE, BOARD_SIZE, configuration)
     val expectedMovesForBlack = Set(
-      Position(left - STEP, left),
-      Position(left, left - STEP),
-      Position(right + STEP, right + STEP + STEP),
-      Position(right + STEP, right)
+      Position(left - DISTANCE, left),
+      Position(left, left - DISTANCE),
+      Position(right + DISTANCE, right + DISTANCE + DISTANCE),
+      Position(right + DISTANCE, right)
     )
     val availableMoves = board.getAvailableMoves(Color.Black)
     val diffBetweenAvailableAndExpected = availableMoves diff expectedMovesForBlack
