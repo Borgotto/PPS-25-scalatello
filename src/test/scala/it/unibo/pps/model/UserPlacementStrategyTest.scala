a000aec12cb50e323563066b282dae50c8f32584
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-01 21:08:36 +0200

    Refactor tests to be more idiomatic
    following ScalaTest's FlatSpec documentation.
    Also remove unused/extra imports.

--- src/test/scala/it/unibo/pps/model/UserPlacementStrategyTest.scala
+++ src/test/scala/it/unibo/pps/model/UserPlacementStrategyTest.scala
@@ -3,6 +3,7 @@ package it.unibo.pps.model
 import org.scalatest.flatspec.AnyFlatSpec
 import org.mockito.MockitoSugar.mock
 import org.mockito.IdiomaticMockito.returns
+import org.scalatest.matchers.should.Matchers.shouldEqual
 
 import it.unibo.pps.model.placementStrategy.UserPlacementStrategy
 import it.unibo.pps.model.player.User
@@ -10,13 +11,9 @@ import it.unibo.pps.utils.Position
 
 class UserPlacementStrategyTest extends AnyFlatSpec:
 
-  val user: User = mock[User]
-  user.getPlacementStrategy returns UserPlacementStrategy()
-
   "UserPlacementStrategy" should "return the user choice" in:
     // The user choice represents the input coming from the view component
     given userChoice: Position = Position(3, 4)
-
-    user.getPlacementStrategy.computePlacement match
-      case result: Position => if result equals userChoice then succeed: Unit
-      case _ => fail(s"The strategy's result is not the expected user choice")
+    val user: User = mock[User]
+    user.getPlacementStrategy returns UserPlacementStrategy()
+    user.getPlacementStrategy.computePlacement shouldEqual userChoice
