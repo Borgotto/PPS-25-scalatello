3b0cc60d3dc5f6ab49814bdd89b5c4e915daee11
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-04 18:42:59 +0200

    Refactor: move User, Opponent classes into Player.
    I've simplified structure by moving the case classes User and Opponent into the Player trait's file.
    Then updated tests accordingly.
    
    Also added color as input parameter to players

--- src/main/scala/it/unibo/pps/model/player/Opponent.scala
+++
@@ -1,9 +0,0 @@
-package it.unibo.pps.model.player
-
-import it.unibo.pps.utils.Color
-import it.unibo.pps.model.placementStrategy.{OpponentPlacementStrategy, RandomOpponentPlacementStrategy}
-
-enum Opponent extends Player:
-  val color: Color = Color.White
-  case RandomOpponent
-    def placementStrategy: OpponentPlacementStrategy = RandomOpponentPlacementStrategy()
--- src/main/scala/it/unibo/pps/model/player/Player.scala
+++ src/main/scala/it/unibo/pps/model/player/Player.scala
@@ -1,8 +1,17 @@
 package it.unibo.pps.model.player
 
 import it.unibo.pps.utils.Color
-import it.unibo.pps.model.placementStrategy.{OpponentPlacementStrategy, UserPlacementStrategy}
+import it.unibo.pps.model.placementStrategy.*
 
 trait Player:
   def color: Color
   def placementStrategy: UserPlacementStrategy | OpponentPlacementStrategy
+
+case class User(color: Color) extends Player:
+  def placementStrategy: UserPlacementStrategy = UserPlacementStrategy()
+
+enum Opponent extends Player:
+  case RandomOpponent(color: Color)
+  def placementStrategy: OpponentPlacementStrategy =
+    this match
+      case RandomOpponent(_) => RandomOpponentPlacementStrategy()
