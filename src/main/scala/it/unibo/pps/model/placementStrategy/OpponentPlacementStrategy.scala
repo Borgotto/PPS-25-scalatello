a926bf95e5b8f73ba88b9b62b301cd3c8d4036f6
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-04 18:48:44 +0200

    Refactor: put user/opponent classes in trait file.
    Simplifying file structure.

--- src/main/scala/it/unibo/pps/model/placementStrategy/OpponentPlacementStrategy.scala
+++
@@ -1,7 +0,0 @@
-package it.unibo.pps.model.placementStrategy
-
-import it.unibo.pps.utils.Position
-import it.unibo.pps.model.MatchState
-
-abstract case class OpponentPlacementStrategy() extends PlacementStrategy[MatchState, Position]:
-  def computePlacement(using matchState: MatchState): Position
--- src/main/scala/it/unibo/pps/model/placementStrategy/UserPlacementStrategy.scala
+++
@@ -1,7 +0,0 @@
-package it.unibo.pps.model.placementStrategy
-
-import it.unibo.pps.utils.Position
-
-case class UserPlacementStrategy() extends PlacementStrategy[Position, Position]:
-  def computePlacement(using userChoice: Position): Position = userChoice
-
