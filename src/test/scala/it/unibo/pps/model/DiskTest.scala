d3a3dffd450ba673913437dc99fdb17c50a52e0d
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-05 00:14:29 +0200

    Refactor DiskTest

--- src/test/scala/it/unibo/pps/model/DiskTest.scala
+++ src/test/scala/it/unibo/pps/model/DiskTest.scala
@@ -1,15 +1,17 @@
 package it.unibo.pps.model
 
-import org.scalatest.flatspec.AnyFlatSpec
 import it.unibo.pps.utils.Color
 import it.unibo.pps.model.board.Disk
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.{should, be}
 
 class DiskTest extends AnyFlatSpec:
   "A Disk" should "have a color" in:
     val disk = Disk(Color.Black)
-    assert(disk.color == Color.Black)
+    disk.color.equals(Color.Black) should be(true)
 
   "A Disk" should "have the opposite color when flipped" in:
     val disk = Disk(Color.Black)
     val flippedDisk = disk.flip()
-    assert(flippedDisk.color == Color.White)
+    flippedDisk.color.equals(disk.color.opposite) should be(true)
