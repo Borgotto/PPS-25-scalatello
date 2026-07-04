cc5d79bc775b4cf87d281c6272a54f51ef58a04c
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-04 16:42:07 +0200

    Revert to previous shape implementation

--- src/main/scala/it/unibo/pps/utils/Shape.scala
+++ src/main/scala/it/unibo/pps/utils/Shape.scala
@@ -1,5 +1,5 @@
 package it.unibo.pps.utils
 
-enum Shape(val height: Int, val width: Int):
-  case Square(size: Int) extends Shape(size, size)
-  case Rectangle(h: Int, w: Int) extends Shape(h, w)
+enum Shape:
+  case Square(size: Int)
+  case Rectangle(height: Int, width: Int)
