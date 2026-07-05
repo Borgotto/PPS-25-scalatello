01938467834c9de56ae2ef12a4e3b52c2ae2b9de
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-05 13:31:32 +0200

    Refactor tests using TableDrivenPropertyChecks

--- src/test/scala/it/unibo/pps/model/BoardTest.scala
+++ src/test/scala/it/unibo/pps/model/BoardTest.scala
@@ -1,111 +1,134 @@
 package it.unibo.pps.model
 
 import it.unibo.pps.model.board.{Board, Disk}
-import it.unibo.pps.state.{BoardState, DiskState}
-import it.unibo.pps.utils.{Color, Position, Shape}
+import it.unibo.pps.state.BoardState
+import it.unibo.pps.utils.{Color, Shape}
 
-import scala.collection.immutable.HashMap
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.{be, should}
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class BoardTest(shape: Shape) extends AnyFlatSpec:
-  private val DIST: Int = 1
+class BoardTest extends AnyFlatSpec with TableDrivenPropertyChecks:
+  private val BOARD_SIZE: Int = 4
+  private val BOARD_HEIGHT: Int = 4
+  private val BOARD_WIDTH: Int = 6
 
-  private val whiteDisk: Disk = Disk(Color.White)
-  private val blackDisk: Disk = Disk(Color.Black)
-  private val topLeftCenterPos: Position =
-    shape match
-      case Shape.Square(n) => Position(n / 2 - DIST, n / 2 - DIST)
-      case Shape.Rectangle(h, w) => Position(h / 2 - DIST, w / 2 - DIST)
-  private val initialDisksOnBoard: HashMap[Position, Disk] =
-    shape match
-      case _ => HashMap(
-        Position(topLeftCenterPos.row, topLeftCenterPos.column) -> whiteDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST) -> whiteDisk
-      )
-  private val boardDuringMatch: Board =
-    shape match
-      case _ => Board(shape, initialDisksOnBoard ++ HashMap(
-        Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> whiteDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST) -> blackDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST + DIST) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column + DIST + DIST) -> blackDisk
-      ))
-  private val initialBoard: Board = Board(shape)
-  private val validMovePos: Position =
-    shape match
-      case _ => Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST + DIST)
-  private val notValidMovePos: Position =
-    shape match
-      case _ => Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column - DIST)
+  private val squareParams: BoardTestParams = BoardTestParams(Shape.Square(BOARD_SIZE))
+  private val rectangleParams: BoardTestParams = BoardTestParams(Shape.Rectangle(BOARD_HEIGHT, BOARD_WIDTH))
 
-  def creationInitialBoardTest(): Unit =
-    initialBoard.disks should be(initialDisksOnBoard)
-    
-  def creationBoardWithDisksTest(): Unit =
-    initialBoard.disks should be(Board(shape, initialDisksOnBoard).disks)
-    
-  def availableMovesTest(): Unit =
-    val expectedMoves: Set[Position] = 
-      shape match
-        case _ => Set(
-          Position(topLeftCenterPos.row - DIST, topLeftCenterPos.column),
-          Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST),
-          Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST + DIST),
-          Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column + DIST),
-        )
-    val availableMoves: Set[Position] = initialBoard.getAvailableMoves(Color.Black)
-    availableMoves.equals(expectedMoves) should be(true)
-    
-  def validMoveTest(): Unit =
-    boardDuringMatch.isMoveValid(validMovePos, Color.Black) should be(true)
-    
-  def notValidMoveTest(): Unit =
-    boardDuringMatch.isMoveValid(notValidMovePos, Color.Black) should be(false)
-    
-  def equalsTest(): Unit =
-    initialBoard.equals(initialBoard) should be(true)
-    
-  def notEqualTest(): Unit =
-    initialBoard.equals(boardDuringMatch) should be(false)
-  
-  def placeDiskInValidMoveTest(): Unit =
-    val board: Board = boardDuringMatch.placeDisk(validMovePos, Color.Black)
-    val expectedBoard: Board = Board(shape, boardDuringMatch.disks + (validMovePos -> blackDisk))
-    board.equals(expectedBoard) should be(true)
-    
-  def placeDiskInNotValidMoveTest(): Unit =
-    val board: Board = boardDuringMatch.placeDisk(notValidMovePos, Color.Black)
-    board.equals(boardDuringMatch) should be(true)
-    
-  def captureDisksTest(): Unit =
-    val board: Board = boardDuringMatch.placeDisk(validMovePos, Color.Black).captureDisks(validMovePos, Color.Black)
-    val expectedBoard: Board = 
-      shape match
-        case _ => Board(shape, boardDuringMatch.disks ++ HashMap(
-          Position(topLeftCenterPos.row, topLeftCenterPos.column) -> blackDisk,
-          Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST) -> blackDisk,
-          Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST) -> blackDisk,
-          Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST + DIST) -> blackDisk,
-          validMovePos -> blackDisk
-        ))
-    board.equals(expectedBoard) should be(true)
-    
-  def boardStateTest(): Unit =
-    val boardState: BoardState = initialBoard.state
-    val diskStates: Seq[DiskState] = 
-      shape match
-        case _ => Seq(
-          DiskState(whiteDisk.color, Position(topLeftCenterPos.row, topLeftCenterPos.column)),
-          DiskState(blackDisk.color, Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST)),
-          DiskState(blackDisk.color, Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column)),
-          DiskState(whiteDisk.color, Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST))
-        )
-    val expectedState: BoardState = BoardState(shape, diskStates)
-    boardState.shape.equals(expectedState.shape) should be(true)
-    boardState.disks.toSet.equals(expectedState.disks.toSet) should be(true)
+  private val initialBoardsTestTable = Table(
+    ("initialBoard", "initialDisksOnBoard"),
+    (squareParams.initialBoard, squareParams.initialDisksOnBoard),
+    (rectangleParams.initialBoard, rectangleParams.initialDisksOnBoard)
+  )
+  "A Board without disks" should "initialize itself with the disks in the correct positions" in:
+    forEvery(initialBoardsTestTable) { (initialBoard, initialDisksOnBoard) =>
+      initialBoard.disks should be(initialDisksOnBoard)
+    }
+
+  private val creationBoardsTestTable = Table(
+    ("initialBoard", "boardShape", "initialDisksOnBoard"),
+    (squareParams.initialBoard, squareParams.shape, squareParams.initialDisksOnBoard),
+    (rectangleParams.initialBoard, rectangleParams.shape, rectangleParams.initialDisksOnBoard)
+  )
+  "A Board given some disks" should "be created with those disks" in:
+    forEvery(creationBoardsTestTable) { (initialBoard, boardShape, initialDisksOnBoard) =>
+      initialBoard.disks should be(Board(boardShape, initialDisksOnBoard).disks)
+    }
+
+  private val availableMovesTestTable = Table(
+    ("initialBoard", "expectedMoves"),
+    (squareParams.initialBoard, squareParams.expectedAvailableMoves),
+    (rectangleParams.initialBoard, rectangleParams.expectedAvailableMoves)
+  )
+  "A Board" should "know which moves are available for a given player" in:
+    forEvery(availableMovesTestTable) { (initialBoard, expectedMoves) =>
+      initialBoard.getAvailableMoves(Color.Black).equals(expectedMoves) should be(true)
+    }
+
+  private val validMovesTestTable = Table(
+    ("board", "validMovePos"),
+    (squareParams.boardDuringMatch, squareParams.validMovePos),
+    (rectangleParams.boardDuringMatch, rectangleParams.validMovePos)
+  )
+  "A Board" should "know if a move is valid" in:
+    forEvery(validMovesTestTable) { (board, validMovePos) =>
+      board.isMoveValid(validMovePos, Color.Black) should be(true)
+    }
+
+  private val notValidMovesTestTable = Table(
+    ("board", "notValidMovePos"),
+    (squareParams.boardDuringMatch, squareParams.notValidMovePos),
+    (rectangleParams.boardDuringMatch, rectangleParams.notValidMovePos)
+  )
+  "A Board" should "know if a move is not valid" in:
+    forEvery(notValidMovesTestTable) { (board, notValidMovePos) =>
+      board.isMoveValid(notValidMovePos, Color.Black) should be(false)
+    }
+
+  private val equalsTestTable = Table(
+    "board",
+    squareParams.initialBoard,
+    rectangleParams.initialBoard
+  )
+  "A Board" should "know if is equal to another board" in:
+    forEvery(equalsTestTable) { board =>
+      board.equals(board) should be(true)
+    }
+
+  private val notEqualsTestTable = Table(
+    ("board", "notEqualBoard"),
+    (squareParams.initialBoard, squareParams.boardDuringMatch),
+    (rectangleParams.initialBoard, rectangleParams.boardDuringMatch)
+  )
+  "A Board" should "know if is not equal to another board" in:
+    forEvery(notEqualsTestTable) { (board, notEqualBoard) =>
+      board.equals(notEqualBoard) should be(false)
+    }
+
+  private val placeDiskTestTable = Table(
+    ("boardDuringMatch", "boardShape", "validMovePos"),
+    (squareParams.boardDuringMatch, squareParams.shape, squareParams.validMovePos),
+    (rectangleParams.boardDuringMatch, rectangleParams.shape, rectangleParams.validMovePos)
+  )
+  "A Board" should "be able to place a new disk in a valid position" in:
+    forEvery(placeDiskTestTable) { (boardDuringMatch, boardShape, validMovePos) =>
+      val board: Board = boardDuringMatch.placeDisk(validMovePos, Color.Black)
+      val expectedBoard: Board = Board(boardShape, boardDuringMatch.disks + (validMovePos -> Disk(Color.Black)))
+      board.equals(expectedBoard) should be(true)
+    }
+
+  private val notPlaceDiskTestTable = Table(
+    ("boardDuringMatch", "notValidMovePos"),
+    (squareParams.boardDuringMatch, squareParams.notValidMovePos),
+    (rectangleParams.boardDuringMatch, rectangleParams.notValidMovePos)
+  )
+  "A Board, if the move is not valid" should "not place the new disk" in:
+    forEvery(notPlaceDiskTestTable) { (boardDuringMatch, notValidMovePos) =>
+      val board: Board = boardDuringMatch.placeDisk(notValidMovePos, Color.Black)
+      board.equals(boardDuringMatch) should be(true)
+    }
+
+  private val captureDisksTestTable = Table(
+    ("boardDuringMatch", "validMovePos", "expectedBoardAfterCapture"),
+    (squareParams.boardDuringMatch, squareParams.validMovePos, squareParams.expectedBoardAfterCapture),
+    (rectangleParams.boardDuringMatch, rectangleParams.validMovePos, rectangleParams.expectedBoardAfterCapture)
+  )
+  "A Board, after placing a disk" should "capture the correct disks" in:
+    forEvery(captureDisksTestTable) { (boardDuringMatch, validMovePos, expectedBoardAfterCapture) =>
+      val board: Board = boardDuringMatch.placeDisk(validMovePos, Color.Black).captureDisks(validMovePos, Color.Black)
+      board.equals(expectedBoardAfterCapture) should be(true)
+    }
+
+  private val boardStateTestTable = Table(
+    ("initialBoard", "boardShape", "expectedDiskStates"),
+    (squareParams.initialBoard, squareParams.shape, squareParams.expectedDiskStates),
+    (rectangleParams.initialBoard, rectangleParams.shape, rectangleParams.expectedDiskStates)
+  )
+  "A Board" should "return its correct state" in:
+    forEvery(boardStateTestTable) { (initialBoard, boardShape, expectedDiskStates) =>
+      val boardState: BoardState = initialBoard.state
+      val expectedState: BoardState = BoardState(boardShape, expectedDiskStates)
+      boardState.shape.equals(expectedState.shape) should be(true)
+      boardState.disks.toSet.equals(expectedState.disks.toSet) should be(true)
+    }
