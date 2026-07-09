126c4ac2e37a388f81a20853371585b25e980a9a
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-10 01:43:16 +0200

    Add: handleSelection() function

--- src/main/scala/it/unibo/pps/controller/MatchController.scala
+++ src/main/scala/it/unibo/pps/controller/MatchController.scala
@@ -1,12 +1,13 @@
 package it.unibo.pps.controller
 
 import it.unibo.pps.model.{Logic, LogicImpl}
-import it.unibo.pps.utils.{Color, Shape}
+import it.unibo.pps.utils.{Color, Position, Shape}
 import it.unibo.pps.view.View
 
 trait MatchController:
   private[controller] var logic: Logic
   def startMatch(shape: Shape, userColor: Color): Unit
+  def handleSelection(diskPos: Position): Unit
   
 class MatchControllerImpl(private val view: View) extends MatchController:
   private[controller] var logic: Logic = _
@@ -14,3 +15,13 @@ class MatchControllerImpl(private val view: View) extends MatchController:
   def startMatch(shape: Shape, userColor: Color): Unit =
     logic = LogicImpl(shape, userColor)
     view.update(logic.state)
+
+  def handleSelection(diskPos: Position): Unit =
+    val userColor = logic.state.activePlayer.color
+    logic = logic.placeUserDisk(diskPos)
+    view.update(logic.state)
+    while
+      !logic.state.activePlayer.color.equals(userColor)
+    do
+      logic = logic.placeOpponentDisk()
+      view.update(logic.state)
--- src/test/scala/it/unibo/pps/controller/MatchControllerTest.scala
+++ src/test/scala/it/unibo/pps/controller/MatchControllerTest.scala
@@ -1,7 +1,8 @@
 package it.unibo.pps.controller
 
 import it.unibo.pps.model.LogicImpl
-import it.unibo.pps.utils.{Color, Shape}
+import it.unibo.pps.state.MatchState
+import it.unibo.pps.utils.{Color, Position, Shape}
 import it.unibo.pps.view.View
 
 import org.mockito.MockitoSugar.mock
@@ -11,11 +12,21 @@ import org.scalatest.matchers.should.Matchers.{be, should}
 
 class MatchControllerTest extends AnyFlatSpec:
   private val view: View = mock[View]
-  private val controller: MatchController = MatchControllerImpl(view)
 
   "A controller, to start a match" should "instantiate the correct Logic object" in:
     val boardSize = 4
     val boardShape: Shape = Shape.Square(boardSize)
     val userColor: Color = Color.Black
+    val controller: MatchController = MatchControllerImpl(view)
     controller.startMatch(boardShape, userColor)
     controller.logic.state.equals(LogicImpl(boardShape, userColor).state) should be(true)
+
+  "A controller" should "handle correctly the selected position and the opponent turn accordingly, " +
+    "so the active player should be the user again" in:
+    val boardSize = 4
+    val boardShape: Shape = Shape.Square(boardSize)
+    val userColor: Color = Color.Black
+    val controller: MatchController = MatchControllerImpl(view)
+    controller.startMatch(boardShape, userColor)
+    controller.handleSelection(Position(boardSize/2 - 1, boardSize/2 - 1 - 1))
+    controller.logic.state.activePlayer.color should be(userColor)
