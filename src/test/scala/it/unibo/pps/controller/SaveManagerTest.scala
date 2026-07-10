e04cdd34525178156d47f69aaa7c7aee682721b6
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-10 22:20:12 +0200

    Refactor: create shared tests for SaveManager
    also made them a lot more idiomatic

--- src/test/scala/it/unibo/pps/controller/SaveManagerTest.scala
+++ src/test/scala/it/unibo/pps/controller/SaveManagerTest.scala
@@ -1,30 +1,46 @@
 package it.unibo.pps.controller
 
+import os.{Path, read, temp, write}
+import it.unibo.pps.state.{BoardState, MatchState, PlayerState}
+import it.unibo.pps.utils.{Color, MatchStatus, Shape}
+import it.unibo.pps.model.strategy.UserPlacementStrategy
+
 import it.unibo.pps.controller.saveManager.*
+import it.unibo.pps.utils.Serializer.*
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.{shouldBe, be, noException}
+import org.scalatest.matchers.should.Matchers.{be, noException, shouldBe}
+import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor3}
 
-class SaveManagerTest extends AnyFlatSpec:
+class SaveManagerTest extends AnyFlatSpec with TableDrivenPropertyChecks:
 
-  trait SaveManagerFactory:
-    val tmpFile: os.Path = os.temp()
-    val saveManager: SaveManager = SaveManagerImpl(tmpFile)
-    val testData: String = "test input"
-    given Saveable[String] with
-        def encode(data: String): String = data
-        def decode(data: String): String = data
+  private val tmpFile: Path = temp()
+  private val inputs: TableFor3[SaveManager[?], Serializer[?], ?] = Table(
+    ("an instance of SaveManager", "its serializer", "some test data"),
+    (StringSaveManager(tmpFile), StringSerializer, "Test string"),
+    (MatchStateSaveManager(tmpFile), MatchSerializer, MatchState(MatchStatus.InProgress,PlayerState.User(Color.Black, UserPlacementStrategy()), BoardState(Shape.Square(5), Seq.empty, Set.empty)))
+  )
 
-  "SaveManager" should "save data correctly" in new SaveManagerFactory:
-    saveManager.save(testData)
-    os.read(tmpFile) shouldBe testData
+  behave like saveManager (using inputs)
 
-  it should "load data correctly" in new SaveManagerFactory:
-    os.write.over(tmpFile, testData)
-    saveManager.load() shouldBe testData
+  // Shared tests for SaveManager instances
+  def saveManager[C](using inputs: TableFor3[SaveManager[?], Serializer[?], C]): Unit =
+    forEvery(inputs): (sm, se, data) =>
+        val saveManager = sm.asInstanceOf[SaveManager[C]]
+        val serializer = se.asInstanceOf[Serializer[C]]
+        val encoded = serializer.encode
+        val testSubject = saveManager.getClass.getSimpleName
 
-  it should "not throw an exception when saving data" in new SaveManagerFactory:
-    noException should be thrownBy saveManager.save(testData)
+        testSubject should "save data correctly" in:
+          saveManager.save(data)
+          read(saveManager.filePath) shouldBe encoded(data)
 
-  it should "not throw an exception when loading data" in new SaveManagerFactory:
-    noException should be thrownBy saveManager.load()
+        it should "load data correctly" in:
+          write.over(saveManager.filePath, encoded(data))
+          saveManager.load shouldBe data
+
+        it should "not throw an exception when saving data" in:
+          noException should be thrownBy saveManager.save
+
+        it should "not throw an exception when loading data" in:
+          noException should be thrownBy saveManager.load
