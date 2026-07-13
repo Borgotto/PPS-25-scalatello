6882e4fda15822a7bc122606cd70ef9e6fb15fbb
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-14 01:47:05 +0200

    Refactor: MatchControllerTest
    - remove a test no more useful
    - refactor the other tests using mockito to remove the access to the logic field in the MatchController class
    - now the tests check how many times a specific method is called

--- src/test/scala/it/unibo/pps/controller/MatchControllerTest.scala
+++ src/test/scala/it/unibo/pps/controller/MatchControllerTest.scala
@@ -1,112 +1,33 @@
 package it.unibo.pps.controller
 
-import it.unibo.pps.model.board.{Board, Disk}
-import it.unibo.pps.model.Logic
-import it.unibo.pps.model.strategy.UserPlacementStrategy
-import it.unibo.pps.state.{BoardState, DiskState, MatchState, PlayerState}
-import it.unibo.pps.utils.{Color, MatchStatus, Position, Shape}
-import it.unibo.pps.view.View
+import it.unibo.pps.state.MatchState
+import it.unibo.pps.utils.{Color, Position, Shape}
 
-import org.mockito.MockitoSugar.mock
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito
+import org.mockito.Mockito.{times, verify}
 
-import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.{be, should}
 
-class MatchControllerTest extends AnyFlatSpec with PrivateMethodTester:
+class MatchControllerTest extends AnyFlatSpec:
   private val BOARD_SIZE = 4
   private val BOARD_SHAPE: Shape = Shape.Square(BOARD_SIZE)
-  private val USER_COLOR: Color = Color.Black
 
-  private val topLeftCenterPos = Position(BOARD_SIZE/2 - 1, BOARD_SIZE/2 - 1)
-  private val dist = 1
+  private val topLeftCenterPos: Position = Position(BOARD_SIZE / 2 - 1, BOARD_SIZE / 2 - 1)
+  private val validPos: Position = Position(topLeftCenterPos.row, topLeftCenterPos.column - 1)
 
-  private val view: View = mock[View]
-  private val controller: MatchController = MatchController(view)
-  controller.startMatch(BOARD_SHAPE, USER_COLOR)
+  "A Controller, if the first player is the User" should "notify the state of the match only once" in:
+    val mockedController: MatchControllerImpl = Mockito.spy(MatchControllerImpl())
+    mockedController.startMatch(BOARD_SHAPE, Color.Black)
+    verify(mockedController, times(1)).notifySubscribers(any(classOf[MatchState]))
 
-  extension (s: MatchState)
-    private def equalsToState(state: MatchState): Boolean =
-      s.status.equals(state.status) &&
-        s.activePlayer.color.equals(state.activePlayer.color) &&
-        s.activePlayer.strategy.equals(state.activePlayer.strategy) &&
-        s.board.shape.equals(state.board.shape) &&
-        s.board.disks.toSet.equals(state.board.disks.toSet) &&
-        s.board.userAvailablePlacements.equals(state.board.userAvailablePlacements)
+  "A Controller, if the first player is the opponent" should "notify the state of the match twice" in:
+    val mockedController: MatchControllerImpl = Mockito.spy(MatchControllerImpl())
+    mockedController.startMatch(BOARD_SHAPE, Color.White)
+    verify(mockedController, times(2)).notifySubscribers(any(classOf[MatchState]))
 
-  "A Controller, to start a match" should "instantiate the correct Logic object" in:
-    controller.logic.state.equalsToState(Logic(BOARD_SHAPE, USER_COLOR).state) should be(true)
-
-  "A Controller, if the first player is the opponent" should "handle its turn, " +
-    "instantiating the Logic object accordingly" in:
-    val controllerOpponent: MatchController = MatchController(view)
-    val userColor: Color = Color.White
-    controllerOpponent.logic = Logic(userColor,
-      Board(BOARD_SHAPE, Map(
-          Position(topLeftCenterPos.row - dist, topLeftCenterPos.column) -> Disk(Color.White),
-          topLeftCenterPos -> Disk(Color.White),
-          Position(topLeftCenterPos.row, topLeftCenterPos.column + dist) -> Disk(Color.Black)
-        )
-      )
-    )
-    val expectedState: MatchState =
-      MatchState(MatchStatus.InProgress,
-        PlayerState.User(userColor, UserPlacementStrategy()),
-        BoardState(BOARD_SHAPE, Seq(
-            DiskState(Color.White, Position(topLeftCenterPos.row - dist, topLeftCenterPos.column)),
-            DiskState(Color.Black, topLeftCenterPos),
-            DiskState(Color.Black,  Position(topLeftCenterPos.row, topLeftCenterPos.column + dist)),
-            DiskState(Color.Black, Position(topLeftCenterPos.row, topLeftCenterPos.column - dist))
-          ), Set(
-            Position(topLeftCenterPos.row + dist, topLeftCenterPos.column),
-            Position(topLeftCenterPos.row + dist, topLeftCenterPos.column + dist + dist)
-          )
-        )
-      )
-    val handleOpponentTurn = PrivateMethod[Unit](Symbol("handleOpponentTurn"))
-    controllerOpponent invokePrivate handleOpponentTurn()
-    controllerOpponent.logic.state.equalsToState(expectedState) should be(true)
-
-  "A Controller" should "handle correctly the selected position and the opponent turn, " +
-    "instantiating the Logic object accordingly" in:
-    val expectedState: MatchState =
-      MatchState(MatchStatus.InProgress,
-        PlayerState.User(USER_COLOR, UserPlacementStrategy()),
-        BoardState(BOARD_SHAPE,
-          Seq(
-            DiskState(Color.White, Position(topLeftCenterPos.row - dist, topLeftCenterPos.column - dist)),
-            DiskState(Color.White, topLeftCenterPos),
-            DiskState(Color.Black, Position(topLeftCenterPos.row, topLeftCenterPos.column + dist)),
-            DiskState(Color.Black, Position(topLeftCenterPos.row, topLeftCenterPos.column + dist + dist)),
-            DiskState(Color.White, Position(topLeftCenterPos.row + dist, topLeftCenterPos.column + dist))
-          ), Set(
-            Position(topLeftCenterPos.row, topLeftCenterPos.column - dist),
-            Position(topLeftCenterPos.row + dist + dist, topLeftCenterPos.column),
-            Position(topLeftCenterPos.row + dist + dist, topLeftCenterPos.column + dist)
-          )
-        )
-      )
-    controller.logic = Logic(USER_COLOR,
-      Board(BOARD_SHAPE, Map(
-          Position(topLeftCenterPos.row - dist, topLeftCenterPos.column - dist) -> Disk(Color.White),
-          Position(topLeftCenterPos.row, topLeftCenterPos.column + dist) -> Disk(Color.White),
-          Position(topLeftCenterPos.row, topLeftCenterPos.column + dist + dist) -> Disk(Color.Black)
-        )
-      )
-    )
-    println(controller.logic.state)
-    val validPos = Position(topLeftCenterPos.row, topLeftCenterPos.column)
-    controller.handleSelection(validPos)
-    println(controller.logic.state)
-    println(expectedState)
-    controller.logic.state.equalsToState(expectedState) should be(true)
-
-//  "A Controller" should "instantiate the correct Logic object from a MatchState" in:
-//    val stateToLogic = PrivateMethod[Logic](Symbol("stateToLogic"))
-//    val expectedState: MatchState =
-//      MatchState(MatchStatus.InProgress,
-//        PlayerState.User(USER_COLOR, UserPlacementStrategy()),
-//        BoardState(BOARD_SHAPE, Seq(DiskState(Color.Black, Position(0, 0))), Set())
-//      )
-//    val logic = Logic(expectedState)
-//    logic.state.equalsToState(expectedState) should be(true)
+  "A Controller" should "notify the state of the match thrice, at the start, after user selection and opponent turn" in:
+    val mockedController: MatchControllerImpl = Mockito.spy(MatchControllerImpl())
+    mockedController.startMatch(BOARD_SHAPE, Color.Black)
+    mockedController.handleSelection(validPos)
+    verify(mockedController, times(3)).notifySubscribers(any(classOf[MatchState]))
