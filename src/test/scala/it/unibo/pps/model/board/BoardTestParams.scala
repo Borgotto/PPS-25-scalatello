922f6578d6f2ea6991a013083441900deb4f3019
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-14 21:53:41 +0200

    Refactor BoardTestParams
    - change board creation using the toBoard function defined in testUtils

--- src/test/scala/it/unibo/pps/model/board/BoardTestParams.scala
+++ src/test/scala/it/unibo/pps/model/board/BoardTestParams.scala
@@ -3,6 +3,7 @@ package it.unibo.pps.model.board
 import it.unibo.pps.model.board.{Board, Disk}
 import it.unibo.pps.state.DiskState
 import it.unibo.pps.utils.{Color, Position, Shape}
+import it.unibo.pps.testutils.TestExtensions.toBoard
 
 class BoardTestParams(val shape: Shape):
   private val DIST: Int = 1
@@ -21,18 +22,21 @@ class BoardTestParams(val shape: Shape):
         Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> blackDisk,
         Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST) -> whiteDisk
       )
+  val initialBoard: Board = Board(shape)
   val boardDuringMatch: Board =
     shape match
-      case _ => Board(shape, initialDisksOnBoard ++ Map(
-        Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> whiteDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST) -> blackDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST + DIST) -> whiteDisk,
-        Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column + DIST + DIST) -> blackDisk
-      ))
-  val initialBoard: Board = Board(shape)
+      case Shape.Square(_) => """
+        ....
+        BWW.
+        .WWW
+        .B.B
+      """.toBoard
+      case Shape.Rectangle(_,_) => """
+|       ......
+|       .BWW..
+|       ..WWW.
+|       ..B.B.
+|     """.toBoard
   val validMovePos: Position =
     shape match
       case _ => Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST + DIST)
@@ -49,13 +53,18 @@ class BoardTestParams(val shape: Shape):
       )
   val expectedBoardAfterCapture: Board =
     shape match
-      case _ => Board(shape, boardDuringMatch.disks ++ Map(
-        Position(topLeftCenterPos.row, topLeftCenterPos.column) -> blackDisk,
-        Position(topLeftCenterPos.row, topLeftCenterPos.column + DIST) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST) -> blackDisk,
-        Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column + DIST + DIST) -> blackDisk,
-        validMovePos -> blackDisk
-      ))
+      case Shape.Square(_) => """
+        ....
+        BBBB
+        .WBB
+        .B.B
+      """.toBoard
+      case Shape.Rectangle(_,_) => """
+|       ......
+|       .BBBB.
+|       ..WBB.
+|       ..B.B.
+|     """.toBoard
   val expectedDiskStates: Seq[DiskState] =
     shape match
       case _ => Seq(
@@ -66,26 +75,35 @@ class BoardTestParams(val shape: Shape):
       )
   val outOfBoundsTestBoard: Board =
     shape match
-      case Shape.Square(_) => Board(shape, initialDisksOnBoard ++ Map(
-          Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST) -> whiteDisk,
-          Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> whiteDisk,
-          Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column + DIST) -> whiteDisk
-        ))
-      case Shape.Rectangle(_, _) => Board(shape, initialDisksOnBoard ++ Map(
-          Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST) -> whiteDisk,
-          Position(topLeftCenterPos.row, topLeftCenterPos.column - DIST - DIST) -> whiteDisk,
-          Position(topLeftCenterPos.row + DIST, topLeftCenterPos.column) -> whiteDisk,
-          Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column + DIST) -> whiteDisk
-        ))
+      case Shape.Square(_) => """
+        ....
+        WWB.
+        .WW.
+        ..W.
+      """.toBoard
+      case Shape.Rectangle(_,_) => """
+ |      ......
+ |      WWWB..
+ |      ..WW..
+ |      ...W..
+ |    """.toBoard
   val expectedNotOutOfBoundsMoves: Set[Position] =
     shape match
       case _ => Set(Position(topLeftCenterPos.row + DIST + DIST, topLeftCenterPos.column - DIST))
   val notAfterSameColorTestBoard: Board =
     shape match
-      case _ => Board(shape, initialDisksOnBoard ++ Map(
-        Position(topLeftCenterPos.row - DIST, topLeftCenterPos.column) -> blackDisk,
-        Position(topLeftCenterPos.row - DIST, topLeftCenterPos.column - DIST) -> whiteDisk
-      ))
+      case Shape.Square(_) => """
+        WB..
+        .WB.
+        .BW.
+        ....
+      """.toBoard
+      case Shape.Rectangle(_,_) => """
+ |      .WB...
+ |      ..WB..
+ |      ..BW..
+ |      ......
+ |    """.toBoard
   val expectedNotAfterSameColorMoves: Set[Position] =
     shape match
       case Shape.Square(_) => Set(
