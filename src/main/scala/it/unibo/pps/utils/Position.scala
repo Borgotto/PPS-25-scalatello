16c86f7a82f7f2392e5b70dbf308a35701678903
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-15 00:04:19 +0200

    Add implicit conversions

--- src/main/scala/it/unibo/pps/utils/Position.scala
+++ src/main/scala/it/unibo/pps/utils/Position.scala
@@ -3,3 +3,14 @@ package it.unibo.pps.utils
 case class Position(row: Int, column: Int):
   def -(pos: Position): Position =
     Position(this.row - pos.row, this.column - pos.column)
+
+object Position:
+  given Conversion[(Int, Int), Position] = (x, y) =>
+    Position(x, y)
+
+  given Conversion[(String, String), Position] = (x, y) =>
+    (x.toInt, y.toInt)
+
+  given Conversion[String, Position] = s =>
+    val parts = s.split(",").map(_.trim)
+    (parts(0), parts(1))
