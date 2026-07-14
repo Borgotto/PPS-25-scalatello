6639c17f2edbd508157dd5ce5880449f5142de9b
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-14 15:26:37 +0200

    Move extension methods from BoardComputations
    - move - to the Position case class
    - move the others to PositionComputationsExtensions object

---
+++ src/main/scala/it/unibo/pps/model/board/PositionComputationsExtensions.scala
@@ -0,0 +1,57 @@
+package it.unibo.pps.model.board
+
+import it.unibo.pps.utils.{Position, Shape}
+
+import scala.annotation.tailrec
+import scala.math.Ordering.Int
+
+object PositionComputationsExtensions:
+  @tailrec
+  private def getPosOnSameDiagonal(source: Position, destination: Position,
+                                   direction: Position, acc: Set[Position] = Set()): Set[Position] =
+    val nextPos: Position = source - direction
+    (nextPos, destination) match
+      case (f, s) if f.equals(s) => acc
+      case (f, s) => getPosOnSameDiagonal(f, s, direction, acc + f)
+      
+  extension (x: Int)
+    private def inRange(y: Int, z: Int): Boolean =
+      x <= Int.max(y, z) && x >= Int.min(y, z)
+
+    private def inBetween(y: Int, z: Int): Boolean =
+      x.inRange(Int.min(y, z) + 1, Int.max(y, z) - 1)
+
+  extension (p: Position)
+    private def /(pos: Position): Position =
+      (pos.row, pos.column) match
+        case (r, c) if r.equals(0) && c.equals(0) => Position(r, c)
+        case (r, c) if r.equals(0) => Position(r, p.column / c)
+        case (r, c) if c.equals(0) => Position(p.row / r, c)
+        case (_, _) => Position(p.row / pos.row, p.column / pos.column)
+
+    private def onSameDiagonal(firstPos: Position, secondPos: Position): Boolean =
+      val distance: Position = firstPos - secondPos
+      val direction: Position = distance / Position(distance.row.abs, distance.column.abs)
+      getPosOnSameDiagonal(firstPos, secondPos, direction).contains(p) &&
+        distance.row.abs.equals(distance.column.abs)
+    
+    def inBounds(shape: Shape): Boolean =
+      val minPosition: Position = Position(0, 0)
+      shape match
+        case Shape.Square(size) =>
+          p.row.inRange(minPosition.row, size - 1) &&
+            p.column.inRange(minPosition.column, size - 1)
+        case Shape.Rectangle(height, width) =>
+          p.row.inRange(minPosition.row, height - 1) &&
+            p.column.inRange(minPosition.column, width - 1)
+
+    def inBetween(firstPos: Position, secondPos: Position): Boolean =
+      (firstPos, secondPos) match
+        case (f, s) if f.row.equals(s.row) => p.column.inBetween(f.column, s.column) && p.row.equals(f.row)
+        case (f, s) if f.column.equals(s.column) => p.row.inBetween(f.row, s.row) && p.column.equals(f.column)
+        case (f, s) => p.onSameDiagonal(f, s)
+    
+    def inNeighbourhood(pos: Position): Boolean =
+      val maxDistance: Int = 1
+      val distance: Position = p - pos
+      distance.row.abs <= maxDistance && distance.column.abs <= maxDistance
--- src/main/scala/it/unibo/pps/utils/Position.scala
+++ src/main/scala/it/unibo/pps/utils/Position.scala
@@ -1,3 +1,5 @@
 package it.unibo.pps.utils
 
-case class Position(row: Int, column: Int)
+case class Position(row: Int, column: Int):
+  def -(pos: Position): Position =
+    Position(this.row - pos.row, this.column - pos.column)
