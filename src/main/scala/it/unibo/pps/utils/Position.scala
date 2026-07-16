1976553194c8d21e5163e5b6210b0b2f72adf085
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-17 01:40:18 +0200

    Add scaladoc to Position case class and object

--- src/main/scala/it/unibo/pps/utils/Position.scala
+++ src/main/scala/it/unibo/pps/utils/Position.scala
@@ -2,14 +2,51 @@ package it.unibo.pps.utils
 
 import upickle.default.ReadWriter
 
+/** Represents a two-dimensional position.
+ *
+ * It has methods to:
+ * - subtract a position from another [[-]];
+ * - get the position on the: [[left]], [[right]] or [[up]] and [[down]].
+ *
+ * @param row the row of a [[Board]]
+ * @param column the column of a [[Board]]
+ */
 case class Position(row: Int, column: Int) derives ReadWriter:
+  /**
+   * @param pos the position to subtract.
+   * @return the subtraction of this [[Position]] and another.
+   */
   def -(pos: Position): Position = Position(this.row - pos.row, this.column - pos.column)
+
+  /** @return the position to the left of this */
   def left: Position = Position(row, column - 1)
+
+  /** @return the position to the right of this */
   def right: Position = Position(row, column + 1)
+  /** @return the position on top of this */
   def up: Position = Position(row - 1, column)
+  /** @return the position below this */
   def down: Position = Position(row + 1, column)
+  /** @inheritdoc */
   override def toString: String = s"($row,$column)"
 
+/** Contains three implicit conversions:
+ * - from [[scala.Tuple2]] of ([[int]],[[Int]]) to [[Position]]
+ * - from [[scala.Tuple2]] of ([[String]],[[String]]) to [[Position]]
+ * - from [[String]] to [[Position]]
+ *
+ * @example {{{
+ *  import it.unibo.pps.utils.Position
+ *  
+ *  first conversion:
+ *  val pos: Position = (0,0)
+ *
+ *  second conversion:
+ *  val pos: Position = ("0","0")
+ *
+ *  third conversion:
+ *  val pos: Position = "(0,0)" }}}
+ */
 object Position:
   given Conversion[(Int, Int), Position] = (x, y) =>
     Position(x, y)
