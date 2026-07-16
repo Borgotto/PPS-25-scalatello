0559d0f284a008f59988dfff248156f8528117bd
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-16 18:09:59 +0200

    Change visibility of Disk class

--- src/main/scala/it/unibo/pps/model/board/Disk.scala
+++ src/main/scala/it/unibo/pps/model/board/Disk.scala
@@ -2,5 +2,5 @@ package it.unibo.pps.model.board
 
 import it.unibo.pps.utils.Color
 
-private[board] case class Disk(color: Color):
+case class Disk(color: Color):
   def flip(): Disk = Disk(color.opposite)
