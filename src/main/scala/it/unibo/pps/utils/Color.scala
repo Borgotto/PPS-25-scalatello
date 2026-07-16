3b24f51a0bc0a51b451395cef0fbc3800cc89851
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-16 22:47:14 +0200

    Add Color scaladoc

--- src/main/scala/it/unibo/pps/utils/Color.scala
+++ src/main/scala/it/unibo/pps/utils/Color.scala
@@ -2,10 +2,17 @@ package it.unibo.pps.utils
 
 import upickle.default.ReadWriter
 
+/** Enum to represent the possible colors of disks and players.
+ *
+ * It also contains a method to return the [[opposite]] color.
+ *
+ * Possible values: [[Black]], [[White]].
+ */
 enum Color derives ReadWriter:
   case Black
   case White
 
+  /** @return the opposite color: [[Black]] -> [[White]] and vice versa. */
   def opposite: Color = this match
     case Black => White
     case White => Black
