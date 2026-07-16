7e7049abf27edb9146f236e4b8d8f48e4912b661
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-16 22:41:05 +0200

    Add scala doc to BoardCreationExtensions

--- src/main/scala/it/unibo/pps/model/board/BoardCreationExtensions.scala
+++ src/main/scala/it/unibo/pps/model/board/BoardCreationExtensions.scala
@@ -2,8 +2,32 @@ package it.unibo.pps.model.board
 
 import it.unibo.pps.utils.{Color, Position}
 
+/** Contains helper `extension methods` to create a [[Board]] */
 object BoardCreationExtensions:
   extension (s: String)
+    /** Extension method of [[String]].
+     *
+     * Transforms a string with a specific pattern in a
+     * [[scala.collection.immutable.Map]] of [[Position]] -> [[Disk]].
+     *
+     * The pattern to follow is: (x,y) -> c where:
+     *    - x and y are any number
+     *    - c is the color of the disk, the accepted ones are w or W for white and b or B for black.
+     *
+     * @example {{{
+     *            """
+     *              (0,0) -> W
+     *              (1,1) -> B
+     *            """.toPosDiskMap
+     *
+     *            results in:
+     *
+     *            Map[Position, Disk](
+     *              Position(0,0) -> Disk(Color.White)
+     *              Position(1,1) -> Disk(Color.Black)
+     *            ) }}}
+     * @return the [[scala.collection.immutable.Map]] created from the string
+     */
     def toPosDiskMap: Map[Position, Disk] =
       val pattern = """(\((?<position>\d+,\s*\d+)\)\s*->\s*(?<color>[A-Za-z]))""".r
       pattern.findAllMatchIn(s).map(w =>
@@ -15,4 +39,7 @@ object BoardCreationExtensions:
       ).toMap
       
   extension (i: Int)
+    /** Extension method of [[Int]]
+     * @return the number divided by 2
+     */
     def half: Int = i/2
