0a43d04ca9b66175db2c7d568a32010a3d41da7f
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-16 23:00:25 +0200

    Add scaladoc to Disk

--- src/main/scala/it/unibo/pps/model/board/Disk.scala
+++ src/main/scala/it/unibo/pps/model/board/Disk.scala
@@ -2,5 +2,12 @@ package it.unibo.pps.model.board
 
 import it.unibo.pps.utils.Color
 
+/** Represents a generic disk.
+ * 
+ * It has a method to [[flip()]] the disk.
+ * 
+ * @param color the color of the disk to create.
+ */
 case class Disk(color: Color):
+  /** @return a disk with the opposite color of the initial one. */
   def flip(): Disk = Disk(color.opposite)
