637741f1975c70eff44bad9a071b2bf46cb5351d
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-17 02:39:50 +0200

    Add scaladoc to PosComputeExtensionsRectangle

--- src/main/scala/it/unibo/pps/model/board/PosComputeExtensionsRectangle.scala
+++ src/main/scala/it/unibo/pps/model/board/PosComputeExtensionsRectangle.scala
@@ -2,6 +2,16 @@ package it.unibo.pps.model.board
 
 import it.unibo.pps.utils.{Position, Shape}
 
+/** Helper class that contains extension methods of [[Int]] and [[Position]] to make computations on a
+ * [[Shape.Rectangle]] [[Board]].
+ *
+ * Extends: [[PosComputeExtensions]]
+ */
 private[board] class PosComputeExtensionsRectangle extends PosComputeExtensions:
-  extension (p: Position) 
+  extension (p: Position)
+    /** @inheritdoc
+     * Overrides: [[PosComputeExtensions.inBounds()]].
+     * @param shape the [[Shape]] of the [[Board]].
+     * @return `true` if the [[Position]] is in the bounds of the [[Board]], `false` otherwise.
+     */
     override def inBounds(shape: Shape): Boolean = super.inBounds(p)(shape)
