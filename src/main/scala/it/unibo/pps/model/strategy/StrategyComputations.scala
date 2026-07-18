6c566846b14fac121253f1f7d21ce5ba504b9cbb
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-18 13:33:40 +0200

    Create SmartOpponent
    Added a new StrategyComputations object that defines the algorithm for a best placement given a board.

---
+++ src/main/scala/it/unibo/pps/model/strategy/StrategyComputations.scala
@@ -0,0 +1,74 @@
+package it.unibo.pps.model.strategy
+
+import it.unibo.pps.model.board.Board
+import it.unibo.pps.utils.{Color, Position}
+
+import scala.util.boundary, boundary.break
+import scala.math.max
+
+object StrategyComputations:
+  extension (board: Board)
+    private def weight(position: Position): Int =
+      val maxRow = board.state.shape.maxRow
+      val maxCol = board.state.shape.maxColumn
+      val row = position.row
+      val col = position.column
+      if
+        // Corners: Highly prized
+        (row == 0 || row == maxRow) &&
+        (col == 0 || col == maxCol)
+        then 10
+      else if
+        // Inner corners: Dangerous
+        (row == 1 || row == maxRow - 1) &&
+        (col == 1 || col == maxCol - 1)
+        then -4
+      else if 
+        // Edges: Defensive positions
+        row == 0 || row == maxRow ||
+        col == 0 || col == maxCol
+        then 2
+      else 1
+
+    private def score(color: Color): Int =
+      board.state.disks.foldLeft(0): (accumulator, disk) =>
+        val diskScore = if disk.color.equals(color) then 1 else -1
+        val positionScore = board.weight(disk.position)
+        accumulator + diskScore + positionScore
+        
+
+    private def placementValue(color: Color, position: Position)
+                              (using currentScore: Int = board.score(color)): Int =
+      val nextBoard = board.placeDisk(color, position)
+      nextBoard.score(color) - currentScore
+
+    private def getOrderedPlacements(color: Color): Seq[Position] =
+      given currentScore: Int = board.score(color)
+      board.getAvailablePlacements(color)
+        .toSeq
+        .sortBy(board.placementValue(color, _))
+        .reverse
+
+  private def negamax(board: Board, depth: Int, color: Color)
+                     (using alpha: Int = Int.MinValue + 1, beta: Int = Int.MaxValue): Int =
+    val nodeIsTerminal = board.getAvailablePlacements(color).isEmpty
+    
+    if depth == 0 || nodeIsTerminal then
+      board.score(color)
+    else
+      val orderedPlacements = board.getOrderedPlacements(color)
+      boundary: // boundary for pruning
+        orderedPlacements.foldLeft(alpha): (currentAlpha, position) =>
+          val newBoard = board.placeDisk(color, position)
+          val value = -negamax(newBoard, depth - 1, color.opposite)(using -beta, -currentAlpha)
+          if value >= beta then
+            break(value) // stop searching this branch
+          max(value, currentAlpha)
+
+  def calculateBestPlacement(color: Color, depth: Int = 1)
+                            (using board: Board): Position =
+    val orderedPlacements = board.getOrderedPlacements(color)
+    val bestPlacement = orderedPlacements.minBy: position =>
+      val newBoard = board.placeDisk(color, position)
+      negamax(newBoard, depth - 1, color.opposite)
+    bestPlacement
--- src/test/scala/it/unibo/pps/model/strategy/OpponentPlacementStrategyTest.scala
+++ src/test/scala/it/unibo/pps/model/strategy/OpponentPlacementStrategyTest.scala
@@ -3,22 +3,88 @@ package it.unibo.pps.model.strategy
 import it.unibo.pps.model.board.Board
 import it.unibo.pps.model.player.Opponent
 import it.unibo.pps.utils.{Color, Position}
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.{contain, should}
-
 import it.unibo.pps.testutils.TestExtensions.*
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.{contain, should, shouldBe}
+import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor4}
 
-class OpponentPlacementStrategyTest extends AnyFlatSpec:
+class OpponentPlacementStrategyTest extends AnyFlatSpec with TableDrivenPropertyChecks:
 
   "RandomOpponentPlacementStrategy" should "return a valid placement" in:
     given board: Board = """
-       ....
-       .WB.
-       .BW.
-       ....
-     """.toBoard
+      ....
+      .WB.
+      .BW.
+      ....
+      """.toBoard
     val opponent = Opponent.RandomOpponent(Color.White)
     val availablePlacements = board.getAvailablePlacements(opponent.color)
     val position: Position = opponent.strategy.computePlacement
     availablePlacements should contain (position)
+
+  private val inputs: TableFor4[Board, Int, Long, Position] = Table(
+    ("board", "depth", "expected time in ms", "expected position"),
+    (
+      """
+      ....
+      .WB.
+      .BW.
+      ....
+      """.toBoard, 3, 100L, Position(2, 0)
+    ),
+    (
+      """
+      ......
+      ..BB..
+      .BWB..
+      ..B...
+      ......
+      ......
+      """.toBoard, 4, 350L, Position(0, 2)
+    ),
+    (
+      """
+      ........
+      ......W.
+      ....BWB.
+      ..BBWB..
+      ..BBW...
+      ..W.W...
+      ....W...
+      ........
+      """.toBoard, 5, 2000L, Position(2, 1)
+    )
+  )
+
+  behave like smartOpponentPerformance (using inputs)
+
+  // Shared tests for SmartOpponentPlacementStrategy performance
+  def smartOpponentPerformance(using inputs: TableFor4[Board, Int, Long, Position]): Unit =
+    forEvery(inputs): (b, depth, expectedTime, expectedMove) =>
+      given Board = b
+      val availablePlacements = b.getAvailablePlacements(Color.White)
+      val opponent = Opponent.SmartOpponent(Color.White, depth)
+      val testSubject = s"SmartOpponentPlacementStrategy" +
+                        " (input #" + inputs.indexOf((b, depth, expectedTime, expectedMove)) + ")"
+
+      testSubject should s"return a valid placement" in:
+        val position: Position = opponent.strategy.computePlacement
+        availablePlacements should contain (position)
+
+      // todo(borghini): replace this test with a simulation of a game between two SmartOpponent where the smartest should win
+      it should "return an advantageous position" in:
+        val position: Position = opponent.strategy.computePlacement
+        position shouldBe expectedMove
+
+      // todo(borghini): find a hardware independent way to test performance
+      it should s"finish within ${expectedTime}ms for ${availablePlacements.size} available moves at depth $depth" in:
+        val numberOfRuns = 3
+        val totalTime = (1 to numberOfRuns).map( _ =>
+          val startTime = System.currentTimeMillis()
+          opponent.strategy.computePlacement
+          val timeTaken = System.currentTimeMillis() - startTime
+          timeTaken
+        )
+        val averageTime = totalTime.sum.toDouble / numberOfRuns
+        println(s"Average time taken: ${averageTime} ms")
+        assert(averageTime < expectedTime, s"SmartOpponentPlacementStrategy took too long on average: ${averageTime} ms")
