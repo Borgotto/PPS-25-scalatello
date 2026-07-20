99f537f83f4704887110f5656d7453748fb611dd
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-07-20 23:04:35 +0200

    Update scaladoc

--- src/main/scala/it/unibo/pps/controller/Controller.scala
+++ src/main/scala/it/unibo/pps/controller/Controller.scala
@@ -1,72 +1,60 @@
 package it.unibo.pps.controller
 
 import it.unibo.pps.controller.save.SaveManager.SaveManagers.MatchStateSaveManager
-import it.unibo.pps.domain.{Color, OpponentType, Position, Shape}
+import it.unibo.pps.domain.{Color, OpponentType, Position, Shape, ActivePlayer, MatchStatus}
 import it.unibo.pps.model.Logic
-import it.unibo.pps.domain.ActivePlayer.Opponent
 import it.unibo.pps.observer.{Publisher, Subscriber}
 import it.unibo.pps.state.MatchState
-import it.unibo.pps.domain.MatchStatus.InProgress
 import it.unibo.pps.view.View
 
 import scala.annotation.tailrec
 import scala.util.{Success, Try}
 import os.Path
 
-/** 
- * Defines the main controller of the application.
+/** Defines the main controller of the application.
+ *
+ *  All methods must be specified by every class using it.
+ *
+ *  Used by: [[ControllerImpl]]
  */
 trait Controller extends Publisher[MatchState]:
-  
-  /** 
-   * Starts and configures a match according to the passed settings.
-   * @param boardShape the [[Shape]] the [[Board]] must have.
-   * @param userColor the [[Color]] assigned to the user.
-   * @param opponentType the type of opponent.
+  /** Starts and configures a match according to the given settings.
+   *  @param boardShape the [[Shape]] the [[Board]] must have.
+   *  @param userColor the [[Color]] assigned to the user.
+   *  @param opponentType the type of opponent.
    */
   def startMatch(boardShape: Shape, userColor: Color, opponentType: OpponentType): Unit
 
-  /** 
-   * Handles the current turn of the user, according to the position selected by them for their next placement.
-   * @param position the position selected by the user.
+  /** Given the position selected by the user, handles their turn.
+   *  @param position the position selected by the user.
    */
   def handleSelection(position: Position): Unit
 
-  /** 
-   * Saves the current state of the match.
-   * @param fileName the name of the save file to create.
-   * @return [[scala.util.Success]] if the save is successful, [[scala.util.Failure]] otherwise.
+  /** Saves the current state of the match.
+   *  @param fileName the name of the save file.
+   *  @return [[scala.util.Success]] if the save is successful, [[scala.util.Failure]] otherwise.
    */
   def saveMatch(fileName: String): Try[_]
 
-  /** 
-   * Loads a match from a save file.
-   * @param fileName the name of the save file from which the match must be loaded.
-   * @return [[scala.util.Success]] if the match is loaded correctly, [[scala.util.Failure]] otherwise.
+  /** Loads a match from a save file.
+   *  @param fileName the name of the save file from which the match must be loaded.
+   *  @return [[scala.util.Success]] if the match is loaded correctly, [[scala.util.Failure]] otherwise.
    */
   def loadMatch(fileName: String): Try[_]
 
-  /**
-   * @return the names of the save files.
-   */
+  /** @return the names of the save files. */
   def saveFileNames: Seq[String]
 
-  /** 
-   * Deletes a save file.
-   * @param fileName the name of the save file to delete.
-   * @return [[scala.util.Success]] if the file is deleted successfully, [[scala.util.Failure]] otherwise.
+  /** Deletes a save file.
+   *  @param fileName the name of the save file to delete.
+   *  @return [[scala.util.Success]] if the file is deleted successfully, [[scala.util.Failure]] otherwise.
    */
   def deleteSaveFile(fileName: String): Try[_]
 
-/** 
- * Defines factories for [[Controller]] instances.
- */
+/** Factory for [[Controller]] instances. */
 object Controller:
-  
-  /** 
-   * Instantiates a [[Controller]] and then subscribes the [[View]] to the updates
-   * published by the [[Controller]].
-   * @param view the view of the application.
+  /** Instantiates a controller and then subscribes the `view` to the updates published by it.
+   *  @param view the view of the application.
    */
   def apply(view: View): Controller =
     val controller = ControllerImpl()
@@ -75,7 +63,6 @@ object Controller:
 
 /** Implements the controller of the application. */
 private[controller] class ControllerImpl extends Controller:
-
   private var logic: Logic = _
   private var subscribers = Seq[Subscriber[MatchState]]()
 
@@ -91,13 +78,13 @@ private[controller] class ControllerImpl extends Controller:
   def notifySubscribers(state: MatchState): Unit =
     subscribers.foreach(s => s.update(state))
 
-  private def isMatchOver: Boolean = logic.state.status != InProgress
+  private def isMatchOver: Boolean = logic.state.status != MatchStatus.InProgress
 
   @tailrec
   private def handleOpponentTurn(): Unit =
     notifySubscribers(logic.state)
     logic.state.activePlayer match
-      case Opponent if !isMatchOver =>
+      case ActivePlayer.Opponent if !isMatchOver =>
         logic = logic.placeOpponentDisk()
         handleOpponentTurn()
       case _ => ()
@@ -105,9 +92,12 @@ private[controller] class ControllerImpl extends Controller:
   def startMatch(boardShape: Shape, userColor: Color, opponentType: OpponentType): Unit =
     logic = Logic(boardShape, userColor, opponentType)
     logic.state.activePlayer match
-      case Opponent => handleOpponentTurn()
+      case ActivePlayer.Opponent => handleOpponentTurn()
       case _ => notifySubscribers(logic.state)
 
+  /** Given the position selected by the user, handles their turn. Then handles the available opponent's turns.
+   *  @param position the position selected by the user.
+   */
   def handleSelection(position: Position): Unit =
     logic = logic.placeUserDisk(position)
     handleOpponentTurn()
