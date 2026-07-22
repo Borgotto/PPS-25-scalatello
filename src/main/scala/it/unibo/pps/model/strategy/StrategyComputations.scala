33f673c3068682df13e345da13449545e12196b1
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-22 09:35:49 +0200

    Refactor StrategyComputations
    move helper methods to new `StrategyHelper.scala` file to keep `StrategyComputations.scala` clean

--- src/main/scala/it/unibo/pps/model/strategy/StrategyComputations.scala
+++ src/main/scala/it/unibo/pps/model/strategy/StrategyComputations.scala
@@ -2,45 +2,13 @@ package it.unibo.pps.model.strategy
 
 import it.unibo.pps.domain.{Color, Position}
 import it.unibo.pps.model.board.Board
+import it.unibo.pps.model.strategy.StrategyHelper.*
 
 import scala.util.boundary, boundary.break
 import scala.math.max
 import scala.collection.parallel.CollectionConverters.*
 
-object StrategyComputations:
-  extension (board: Board)
-    private def unsafePlaceDisk(color: Color, position: Position): Board =
-      board.placeDisk(color, position, validatePosition = false)
-
-    private def weight(position: Position): Int =
-      val maxRow = board.state.shape.maxRow
-      val maxCol = board.state.shape.maxColumn
-      val row = position.row
-      val col = position.column
-      if
-        // Corners: Highly prized
-        (row == 0 || row == maxRow) &&
-        (col == 0 || col == maxCol)
-        then 10
-      else if
-        // Inner corners: Dangerous
-        (row == 1 || row == maxRow - 1) &&
-        (col == 1 || col == maxCol - 1)
-        then -4
-      else if 
-        // Edges: Defensive positions
-        row == 0 || row == maxRow ||
-        col == 0 || col == maxCol
-        then 2
-      else 1
-
-    private def score(color: Color): Int =
-      board.state.disks.map(disk =>
-        val diskScore = if disk.color.equals(color) then 1 else -1
-        val positionScore = board.weight(disk.position)
-        diskScore + positionScore
-      ).sum
-
+private object StrategyComputations:
   private def negamax(board: Board, depth: Int, color: Color)
                      (using alpha: Int = Int.MinValue + 1, beta: Int = Int.MaxValue): Int =
     val availablePlacements = board.getAvailablePlacements(color)
@@ -51,16 +19,16 @@ object StrategyComputations:
     else
       boundary: // boundary for pruning
         availablePlacements.foldLeft(alpha): (currentAlpha, position) =>
-          val newBoard = board.unsafePlaceDisk(color, position)
+          val newBoard = board.placeDisk(color, position, validatePosition = false)
           val value = -negamax(newBoard, depth - 1, color.opposite)(using -beta, -currentAlpha)
           if value >= beta then
             break(value) // stop searching this branch
           max(value, currentAlpha)
 
-  def calculateBestPlacement(color: Color, depth: Int = 1)
+  def calculateBestPlacement(color: Color, depth: Int)
                             (using board: Board): Position =
     val availablePlacements = board.getAvailablePlacements(color).par
     val bestPlacement = availablePlacements.minBy: position =>
-      val newBoard = board.unsafePlaceDisk(color, position)
+      val newBoard = board.placeDisk(color, position, validatePosition = false)
       negamax(newBoard, depth - 1, color.opposite)
     bestPlacement
