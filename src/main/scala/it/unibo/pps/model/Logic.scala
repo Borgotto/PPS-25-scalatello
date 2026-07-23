28cfb7c0b55c109688db27265198f79ffcfa9d11
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-07-23 18:39:08 +0200

    refactor logic

--- src/main/scala/it/unibo/pps/model/Logic.scala
+++ src/main/scala/it/unibo/pps/model/Logic.scala
@@ -1,13 +1,13 @@
 package it.unibo.pps.model
 
-import it.unibo.pps.domain.{ActivePlayer, Color, MatchStatus, OpponentType, Position, Shape}
-import it.unibo.pps.model.board.Board
-import it.unibo.pps.model.player.{Opponent, User}
-import it.unibo.pps.model.player.Opponent.{EasyOpponent, HardOpponent, MediumOpponent, RandomOpponent, SmartOpponent}
-import it.unibo.pps.state.{BoardState, MatchState}
+import it.unibo.pps.domain.*
 import it.unibo.pps.domain.Color.*
 import it.unibo.pps.domain.MatchStatus.*
 import it.unibo.pps.domain.OpponentType.*
+import it.unibo.pps.model.board.Board
+import it.unibo.pps.model.player.*
+import it.unibo.pps.model.player.Opponent.*
+import it.unibo.pps.state.{BoardState, MatchState}
 
 trait Logic:
   def state: MatchState
@@ -23,45 +23,12 @@ class StandardLogic(
 ) extends Logic:
 
   private val boardState: BoardState = activePlayer match
-    case ActivePlayer.User => board.state.copy(userAvailablePlacements = board.getAvailablePlacements(user.color))
+    case ActivePlayer.User => board.state.copy(
+      userAvailablePlacements = board.getAvailablePlacements(user.color)
+    )
     case ActivePlayer.Opponent => board.state
 
   val state: MatchState = MatchState(status, user, opponent, activePlayer, boardState)
-
-  private def areBothPlayersStuck(board: Board): Boolean =
-    board.getAvailablePlacements(user.color).isEmpty
-    && board.getAvailablePlacements(opponent.color).isEmpty
-
-  private def getUpdatedBoard(newDiskColor: Color, newDiskPosition: Position): Board =
-    board.placeDisk(newDiskColor, newDiskPosition)
-
-  private def getUpdatedStatus(board: Board): MatchStatus =
-    if !areBothPlayersStuck(board) then InProgress
-    else
-      val userDisks = board.state.disks.count(disk => disk.color == user.color)
-      val opponentDisks = board.state.disks.count(disk => disk.color == opponent.color)
-      (userDisks, opponentDisks) match
-        case (a, b) if a > b => UserWon
-        case (a, b) if a < b => OpponentWon
-        case _ => Tie
-
-  private val nextPlayer: ActivePlayer = activePlayer match
-    case ActivePlayer.User => ActivePlayer.Opponent
-    case ActivePlayer.Opponent => ActivePlayer.User
-
-  private def activePlayerColor: Color = activePlayer match
-    case ActivePlayer.User => user.color
-    case ActivePlayer.Opponent => opponent.color
-  
-  private def getUpdatedActivePlayer(board: Board): ActivePlayer =
-    val availablePlacements = board.getAvailablePlacements(activePlayerColor.opposite)
-    if availablePlacements.isEmpty then activePlayer else nextPlayer
-
-  private def getUpdatedLogic(newDiskColor: Color, newDiskPosition: Position): Logic =
-    val updatedBoard = getUpdatedBoard(newDiskColor, newDiskPosition)
-    val updatedStatus = getUpdatedStatus(updatedBoard)
-    val updatedActivePlayer = getUpdatedActivePlayer(updatedBoard)
-    new StandardLogic(updatedStatus, user, opponent, updatedActivePlayer, updatedBoard)
 
   def placeUserDisk(position: Position): Logic = activePlayer match
     case ActivePlayer.Opponent => throw IllegalStateException("It is opponent's turn now")
@@ -74,6 +41,34 @@ class StandardLogic(
     case ActivePlayer.Opponent =>
       val position = opponent.strategy.computePlacement(using board)
       getUpdatedLogic(opponent.color, position)
+
+  private def areBothPlayersStuck(board: Board): Boolean =
+    board.getAvailablePlacements(user.color).isEmpty
+    && board.getAvailablePlacements(opponent.color).isEmpty
+
+  private def getUpdatedStatus(board: Board): MatchStatus =
+    if !areBothPlayersStuck(board) then InProgress
+    else
+      val numUserDisks = board.state.disks.count(_.color == user.color)
+      val numOpponentDisks = board.state.disks.count(_.color == opponent.color)
+      (numUserDisks, numOpponentDisks) match
+        case (a, b) if a > b => UserWon
+        case (a, b) if a < b => OpponentWon
+        case _ => Tie
+
+  private def otherPlayerColor: Color = activePlayer match
+    case ActivePlayer.User => opponent.color
+    case ActivePlayer.Opponent => user.color
+  
+  private def getUpdatedActivePlayer(board: Board): ActivePlayer =
+    val availablePlacements = board.getAvailablePlacements(otherPlayerColor)
+    if availablePlacements.isEmpty then activePlayer else activePlayer.next
+
+  private def getUpdatedLogic(newDiskColor: Color, newDiskPosition: Position): Logic =
+    val updatedBoard = board.placeDisk(newDiskColor, newDiskPosition)
+    val updatedStatus = getUpdatedStatus(updatedBoard)
+    val updatedActivePlayer = getUpdatedActivePlayer(updatedBoard)
+    new StandardLogic(updatedStatus, user, opponent, updatedActivePlayer, updatedBoard)
 
 object Logic:
   
