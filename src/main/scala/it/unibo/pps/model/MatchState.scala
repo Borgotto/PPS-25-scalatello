dbd55b53fb750a78f28c40d79a39f62bbbe14f9b
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-07-25 00:15:06 +0200

    Delete old MatchState
    how did this even get here lol

--- src/main/scala/it/unibo/pps/model/MatchState.scala
+++
@@ -1,9 +0,0 @@
-package it.unibo.pps.model
-
-import it.unibo.pps.model.board.Board
-import it.unibo.pps.model.player.*
-
-abstract class MatchState:
-  def getStatus: Any
-  def getActivePlayer: Player
-  def getBoard: Board
